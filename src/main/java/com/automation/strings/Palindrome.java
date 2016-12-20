f73c6ace4a0546705d76f6e7fbd38a020de5bb3c
raghavender <raghavender.nagabandi@spire2grow.com>
2016-12-20 07:13:39 +0530

    updated with string palindrome

---
+++ src/main/java/com/automation/strings/Palindrome.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.automation.strings;
+
+import java.util.HashMap;
+
+import com.thoughtworks.selenium.webdriven.commands.IsAlertPresent;
+
+/**
+ * @author Raghavender
+ *
+ */
+public class Palindrome {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(isPalindrome("raghav"));
+		System.out.println(isPalindrome("abba"));
+		System.out.println(isPalindrome("madam"));
+		System.out.println(isPalindrome("mmaad"));
+		
+	}
+
+	public static boolean isPalindrome(String str){
+		
+		HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+		
+		for (int i = 0; i < str.length(); i++) {
+			
+			if (map.get(str.charAt(i))==null){
+				map.put(str.charAt(i), 1);
+			}else{
+				map.remove(str.charAt(i));
+			}
+			
+		}
+		
+		if(map.size()>1)
+		 return false;
+		return true;
+	}
+	
+}
